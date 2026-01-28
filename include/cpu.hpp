c6986c9e0824203e1f9fe9265ad8548745e0523b
Lars De Volder <36573562+larsdevolder@users.noreply.github.com>
2026-01-28 06:00:43 +0100

    Add unit test framework and first test

--- include/cpu.hpp
+++ include/cpu.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <cassert>
+#include <catch2/catch_test_macros.hpp>
 #include <fstream>
 #include <iostream>
-#include <cassert>
 
 #include "state.hpp"
+
+class CpuTestWrapper;
 
 class Cpu {
     Registers regs;
@@ -18,4 +21,26 @@ class Cpu {
         int loadRom(std::istream &rom);
         int loadRom(const char* rom);
         int decode();
+    
+    friend class CpuTestWrapper;
+};
+
+class CpuTestWrapper {
+    public:
+        static Registers& regs(Cpu& cpu) { return cpu.regs; }
+        static Memory& memory(Cpu& cpu) { return cpu.memory; }
+        static uint8_t getreg(int n, Cpu& cpu) {
+            switch (n) {
+                case 0: return cpu.regs.B;
+                case 1: return cpu.regs.C;
+                case 2: return cpu.regs.D;
+                case 3: return cpu.regs.E;
+                case 4: return cpu.regs.H;
+                case 5: return cpu.regs.L;
+                case 7: return cpu.regs.A;
+                default:
+                    FAIL("Invalid register index: " << n);
+                    return 0;  // Keep the compiler happy about return paths
+            }
+        }
 };
